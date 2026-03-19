381a5fb53ee9c761e1ff6d2fe3d43daf01bd4a95
Lilian Denizon <lilian.denizon@gmail.com>
2026-03-19 18:53:21 +0100

    ajout du systèmede réservation

--- reservation.php
+++ reservation.php
@@ -13,18 +13,60 @@
 <br><br>
 <div class="reservations">
         <div class="service-container">
-            <select name="" id="">
-            <?php
-                        // Récupérer TOUS les articles avec leur catégorie et auteur
-                        $sql = "SELECT * from service ;";
-                        $stmt = $pdo->query($sql);
-                        $services = $stmt->fetchAll();
+            <form action="" method="post">
+                <select id="prestation" name='reserv-prestation'>
+                <?php
+                            // Récupérer TOUS les articles avec leur catégorie et auteur
+                            $sql = "SELECT * from service ;";
+                            $stmt = $pdo->query($sql);
+                            $services = $stmt->fetchAll();
 
-                        foreach ($services as $service) {
-                                echo "<option>{$service['nom']} | {$service['duree_minutes']}min | {$service['prix_euros']}$</option>";
-                        }
-                ?>
-                </select>
-            </div>
+                            foreach ($services as $service) {
+                                    echo "<option><p  >{$service['nom']}</p> | {$service['duree_minutes']}min | {$service['prix_euros']}$</option>";
+                            }
+                    ?>
+                    
+                    </select>
+
+                    <input type="date" name="date" class="date">
+                    <select class="choix-heure" name='heure'>
+                        <option>10:30</option>
+                        <option>11:30</option>
+                        <option>14:30</option>
+                        <option>16:30</option>
+                        <option>17:00</option>
+                    </select>
+
+
+
+                    <input type="text" name="nom" placeholder="Votre nom" required>
+
+                    <button type="submit">Réserver</button>
+            </form>
+
+            <?php 
+                if (isset($_POST['reserv-prestation'], $_POST['date'], $_POST['heure'])) {
+
+                    $reserv = $_POST['reserv-prestation'];
+                    $date = $_POST['date'];
+                    $heure = $_POST['heure'];
+                    $statut = "en_attente";
+
+                    $sql = "INSERT INTO reservation (nom_prestation, date_rdv, heure_rdv, statut)
+                            VALUES (:reserv, :date, :heure, :statut)";
+
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute([
+                        ':reserv' => $reserv,
+                        ':date' => $date,
+                        ':heure' => $heure,
+                        ':statut' => $statut
+                    ]);
+                    header("Location: reservation.php");
+                    exit;
+                    
+                }
+            ?>
+        </div>
     </div>
     <?php include 'includes/footer.php'; ?>
